c42473b591209213d39dbd342f75afaab2aceef4
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-24 14:09:49 +0530

    loading & transforming CSV data back to JS objects using streams

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,34 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 
+    && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+
+// Reading CSV data as a stream --> returns raw buffers of bytes
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+        }
+    })
+    .on('error', (error) => {
+        console.log(error);
+    })
+    .on('end', () => {
+        console.log(habitablePlanets.map(planet => planet.kepler_name));
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+    });
+
+module.exports = {
+    planets: habitablePlanets,
+};
