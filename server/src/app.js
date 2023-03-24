3e3b3621eb5b3874f7c5d52a7d68c3b9bc1843fc
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-24 12:46:10 +0530

    added Access-Control-Allow-Origin header to the backend API

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
 // middlewares
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 
 // Routes or End-Points
