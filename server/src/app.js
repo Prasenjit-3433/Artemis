bf23b510a6e786c1079d891499e180408abbf208
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-24 10:22:54 +0530

    enpoint  is ready to be consumed by client

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 
+const planetsRouter = require('./routes/planets/planets.router');
+
 const app = express();
 
 // middlewares
 app.use(express.json());
+
+// Routes or End-Points
+app.use(planetsRouter);
 
 module.exports = app;
