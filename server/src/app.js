07f941c2106257f03200ee56d982b1bafa07be4f
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-25 21:31:43 +0530

    logging requests with morgan

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
