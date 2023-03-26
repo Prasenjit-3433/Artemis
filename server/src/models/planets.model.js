2a5f4ceb8edf67090de06d45fc49db3b66c7145d
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-26 14:00:59 +0530

    modified models - created data access fuctions

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -36,7 +36,11 @@ function loadPlanetsData() {
     });
 }
 
+function getAllPlanets() {
+    return habitablePlanets;
+}
+
 module.exports = {
     loadPlanetsData,
-    planets: habitablePlanets,
+    getAllPlanets
 };
