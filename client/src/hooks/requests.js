4af6c3168b5cae79f9844b58553f1458e52034be
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-28 22:12:53 +0530

    created DELETE /launches/id endpoint & integrated with abort mission functionality in front-end

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -30,9 +30,15 @@ async function httpSubmitLaunch(launch) {
   }
 }
 
+// Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method:"DELETE",
+    });
+  } catch (error) {
+    return { ok: false };
+  }
 }
 
 export {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,4 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launches.model');
+const { getAllLaunches, addNewLaunch, existsLaunchWithId, abortLaunchWithId } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
@@ -29,7 +29,23 @@ function httpAddNewLaunch(req, res) {
     return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+    const launchId = Number(req.params.id);
+
+    // if launch doesn't exist
+    if (!existsLaunchWithId(launchId)) {
+        return res.status(404).json({
+            error: 'Launch not found!'
+        });
+    }
+
+    // if lauch does exist
+    const aborted = abortLaunchWithId(launchId);
+    return res.status(200).json(aborted);
+}
+
 module.exports = {
     httpGetAllLaunches,
     httpAddNewLaunch,
+    httpAbortLaunch,
 };
