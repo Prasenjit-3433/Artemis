f9bc9b264cc40c8cb79f95f569214a4742f7b9af
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-29 22:49:44 +0530

    testing API endpoints for POST /launches for success

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,7 +4,8 @@ const app = require('../../app');
 
 describe('Test GET /launches', () => {
     test('It should respond with 200 success', async () => {
-        const response = await request(app).get('/launches')
+        const response = await request(app)
+            .get('/launches')
             .expect('Content-Type', /json/)
             .expect(200);
     });
@@ -12,8 +13,32 @@ describe('Test GET /launches', () => {
 
 
 describe('POST /launches', () => {
-    test('It should respond with 200 success', () => {
+    const completeLaunchData = {
+        mission: 'USS Enterprice',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+        launchDate: 'January 4, 2028'
+    };
 
+    const launchDataWithoutDate = {
+        mission: 'USS Enterprice',
+        rocket: 'NCC 1701-D',
+        target: 'Kepler-186 f',
+    };
+
+    test('It should respond with 201 created', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send(completeLaunchData)
+        .expect('Content-Type', /json/)
+        .expect(201);
+
+        const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+        const responseDate = new Date(response.body.launchDate).valueOf();
+
+        expect(responseDate).toBe(requestDate);
+        
+        expect(response.body).toMatchObject(launchDataWithoutDate);
     });
 
     test('It should catch missing required properties', () => {});
