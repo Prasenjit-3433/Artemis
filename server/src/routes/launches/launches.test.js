49ec146120395409e1706ce8469f917a2635ba87
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-03-29 21:22:24 +0530

    testing API endpoints for GET /launches

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+
+const app = require('../../app');
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app).get('/launches')
+            .expect('Content-Type', /json/)
+            .expect(200);
+    });
+});
+
+
+describe('POST /launches', () => {
+    test('It should respond with 200 success', () => {
+
+    });
+
+    test('It should catch missing required properties', () => {});
+    test('It should catch invalid dates', () => {});
+});
