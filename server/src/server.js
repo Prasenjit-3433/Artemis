218fa3601ccf3a40b20f6b5e4f36595da874f5e7
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-03 12:14:00 +0530

    ready to connect and query mongodb cluster hosted on Atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const cluster = require('cluster');
+const mongoose = require('mongoose');
 
 const http = require('http');
 
@@ -9,9 +10,21 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:CrLV1crtndpuvnOg@nasacluster.gr6skww.mongodb.net/nasa-api-db?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection is ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
+
     await loadPlanetsData();
 
     server.listen(PORT, () => {
