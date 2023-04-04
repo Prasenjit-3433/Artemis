2bfc197321c09ce46e2aff3e5440fd0ca5349bb5
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-04 10:20:29 +0530

    inserted CSV data to MongoDB Cluster

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 
 const { parse } = require('csv-parse');
 
+const planets = require('./planets.mongo');
+
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet) {
@@ -12,6 +14,20 @@ function isHabitablePlanet(planet) {
     && planet['koi_prad'] < 1.6;
 }
 
+async function savePlanet(planet) {
+    try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name,
+        }, {
+            keplerName: planet.kepler_name,
+        }, {
+            upsert: true
+        });
+    } catch (err) {
+        console.error(`Could not save the planet ${err}`);
+    }
+}
+
 function loadPlanetsData() {
     return new Promise((resolve, reject) => {
         fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
@@ -19,25 +35,27 @@ function loadPlanetsData() {
             comment: '#',
             columns: true
         }))
-        .on('data', (data) => {
+        .on('data', async (data) => {
             if (isHabitablePlanet(data)) {
-                habitablePlanets.push(data);
+                // insert the new document only if no such document is found
+                await savePlanet(data);
             }
         })
         .on('error', (error) => {
             console.log(error);
             reject(error);
         })
-        .on('end', () => {
-            console.log(`${habitablePlanets.length} habitable planets found!`);
+        .on('end', async () => {
+            const countPlanetsFound = (await getAllPlanets()).length;
+            console.log(`${countPlanetsFound} habitable planets found!`);
             resolve();
         });
 
     });
 }
 
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find();
 }
 
 module.exports = {
