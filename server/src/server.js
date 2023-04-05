48b9a1802b300a06c25a8fab964dfafd9138013b
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-05 21:37:42 +0530

    connecting to mongodb cluster separated out into services folder for better reusability

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,18 @@
 const cluster = require('cluster');
-const mongoose = require('mongoose');
-
 const http = require('http');
+
+const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
+const { loadPlanetsData } = require('./models/planets.model');
 
 cluster.schedulingPolicy = cluster.SCHED_RR;
 
-const app = require('./app');
-const { loadPlanetsData } = require('./models/planets.model');
-
 const PORT = process.env.PORT || 8000;
-
-const MONGO_URL = 'mongodb+srv://nasa-api:CrLV1crtndpuvnOg@nasacluster.gr6skww.mongodb.net/nasa-api-db?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection is ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-    console.error(err);
-});
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    await mongoConnect();
 
     await loadPlanetsData();
 
---
+++ server/src/services/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const MONGO_URL = 'mongodb+srv://nasa-api:CrLV1crtndpuvnOg@nasacluster.gr6skww.mongodb.net/nasa-api-db?retryWrites=true&w=majority';
+
+mongoose.connection.once('open', () => {
+    console.log('MongoDB connection is ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+});
+
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL);
+}
+
+module.exports = {
+    mongoConnect,
+};
