f2160082c6e4f1e00e44d0f6ff6c8137c446ebf9
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-07 09:48:36 +0530

    versioning API

--- server/src/app.js
+++ server/src/app.js
@@ -4,8 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.router');
-const launchesRouter = require('./routes/launches/launches.router');
+const api = require('./routes/api');
 
 const app = express();
 
@@ -17,9 +16,8 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Routes or End-Points
-app.use('/planets', planetsRouter);
-app.use('/launches', launchesRouter);
+// API Versions
+app.use('/v1', api);
 
 // Serving the Production-build with Client-Side Routing
 app.get('*', (req, res) => {
