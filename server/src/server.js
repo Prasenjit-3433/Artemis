bbe889dda0b9160753dd9c8d70e4e7afb6a153a5
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-11 22:57:22 +0530

    managing sensitive data with dotenv package

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,8 @@
 const cluster = require('cluster');
 const http = require('http');
 
+require('dotenv').config();
+
 const app = require('./app');
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:CrLV1crtndpuvnOg@nasacluster.gr6skww.mongodb.net/nasa-api-db?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection is ready!');
