69acf82dbd164eda601b570257b50f8232afec1e
Prasenjit Sutradhar <prasenjitsutradhar3433@gmail.com>
2023-04-19 14:13:06 +0530

    load planets data in tests

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,10 +2,12 @@ const request = require('supertest');
 
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Testing Launches Endpoints', () => {
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
